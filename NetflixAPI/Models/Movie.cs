6989c98524838f30bf450bc3c44a364a3b9ec1b5
Emma Boldor <emanuela.boldor@emanuel.ro>
2021-03-17 20:26:06 +0200

    Add project files.

---
+++ NetflixAPI/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetflixAPI.Models
+{
+    public class Movie
+    {
+        public Guid id { get; internal set; }
+        public String name { get; set; }
+        public int duration { get; set; }
+    }
+}
